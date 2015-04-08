0c0ca6d4473669842a1caba9e3ec450e7969eebc
Stephen Stack <Stephen.Stack@digi.com>
2015-04-08 11:56:17 -0500

    lib: add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simpcli",
+    description="Simple command line interfaces",
+    url="https://github.com/swstack/simpcli",
+    author="Stack, Stephen",
+    author_email="ss@stephenstack.com",
+    packages=find_packages(),
+    version='dev'
+)
